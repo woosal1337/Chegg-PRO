3a56ce5dfd0f6b9d33ccd618bbedda9a589616da
Vusal Ismayilov <woosal@protonmail.com>
2020-11-17 03:05:22 +0300

    FINAL

--- Public Account/main.py
+++ Public Account/main.py
@@ -1,25 +1,47 @@
 from selenium import webdriver
 import time
 
+# Not displaying the browser
+option = webdriver.ChromeOptions()
+option.add_argument("headless")
+
 # Specifying the path of my Chrome driver
 browser = webdriver.Chrome("C:\Program Files\chromedriver\chromedriver.exe")
 
-# # Getting to Free Chegg.com Website
+# Getting to Free Chegg.com Website
 url = "https://techlacarte.com/how-to-get-chegg-answers-for-free/"
 browser.get(url)
 time.sleep(8)
 
-userNamePath =
-questionLinkPath =
-emailAddressPath =
-sendButtonPath = 
+# Declaring the path of the needed elements
+userNamePath = browser.find_element_by_xpath('//*[@id="wpforms-13116-field_1"]')
+questionLinkPath = browser.find_element_by_xpath('//*[@id="wpforms-13116-field_4"]')
+emailAddressPath = browser.find_element_by_xpath('//*[@id="wpforms-13116-field_2"]')
+sendButtonPath = browser.find_element_by_xpath('//*[@id="wpforms-submit-13116"]')
+
+userName = input("What is your name? ")
+emailAddress = input(f"Dear, {userName} please write your email address where the answer will be sent: ")
 
 while True:
-    userName = input("What is your name? ")
+    # Asking for the question link from the user
     questionLink = input(f"Dear, {userName} Please paste your Chegg question link and click enter: ")
-    emailAddress = input(f"Dear, {userName} please write your email address where the answer will be sent: ")
-    time.sleep(1)
-    print("Working on it...")
-    for i in range(5):
-        print("Working on it" + i*".")
 
+    # Sending the input one-by-one
+    time.sleep(1)
+    userNamePath.send_keys(userName)
+    time.sleep(0.25)
+    questionLinkPath.send_keys(questionLink)
+    time.sleep(0.25)
+    emailAddressPath.send_keys(emailAddress)
+    time.sleep(0.25)
+
+    # Finally sending all the result, and waiting for the answer
+    sendButtonPath.click()
+
+    for i in range(6):
+        print("Working on it" + i*".")
+        time.sleep(1)
+
+    print("Please check your main within 15-45 minutes to get your answer!")
+    print("Made by Vusal with the help of TchLaCarte <3")
+    browser.refresh()
